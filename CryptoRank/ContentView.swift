33d39fc715697c81d76907c07af8e6a4140313ec
Tamás Csató <aydeen89@gmail.com>
2024-08-05 07:25:00 +0200

    • Initial commit

---
+++ CryptoRank/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoRank
+//
+//  Created by Tamás Csató on 05/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoRank/CryptoRankApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoRankApp.swift
+//  CryptoRank
+//
+//  Created by Tamás Csató on 05/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoRankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
